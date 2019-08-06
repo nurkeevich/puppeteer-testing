8aefed84451eb9b104b5f25942a9b11d2b75b563
K-Bermet <bkalbotoeva@gmail.com>
2019-08-05 23:26:19 -0500

    updated automation tests

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,5 +1,6 @@
 module.exports = {
     LOGIN: "[name='user_login']",
     PASSWORD: "#user_password",
-    SIGNIN: "[name='submit']"
+    SIGNIN: "[name='submit']",
+    FORGOT_YOUR_PASSWORD: "[class='offset3 span6'] > a"
 }
