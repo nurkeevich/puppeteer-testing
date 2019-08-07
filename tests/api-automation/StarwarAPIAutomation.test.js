b4e182508e42d5d89223ae35ba29670e0a1f3c7d
Tilekbek(Taylor) Kadyrov <tkadyrov@rcgdirect.com>
2019-08-07 16:24:52 -0500

    api report created

--- tests/api-automation/StarwarAPIAutomation.test.js
+++ tests/api-automation/StarwarAPIAutomation.test.js
@@ -1,7 +1,5 @@
-const should = require('should');
 const request = require('request');
 const expect = require('chai').expect;
-const util = require('util');
 const baseURL = 'https://swapi.co/api';
 
 describe('return Luke', function () {
@@ -12,10 +10,28 @@ describe('return Luke', function () {
                 let bodyObj = JSON.parse(body);
                 expect(bodyObj.name).to.equal('Luke Skywalker');
                 expect(bodyObj.hair_color).to.equal("blond");
+                expect(bodyObj.skin_color).to.equal('fair');
+                expect(bodyObj.gender).to.equal('male');
                 expect(response.statusCode).to.equal(200);
                 console.log(body);
                 done();
             }
-        )
+        );
+    });
+
+    it('should post user to database', function (done) {
+        request.post(
+            {
+                uri: baseURL + '/people/1'
+            },
+
+            {
+
+            },
+
+            async function (error, response, body) {
+                done();
+            }
+        );
     });
 });
